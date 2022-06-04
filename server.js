444b9d3dd5d2c6bef750f0f978b8abc29269db06
Verified ✔️ <ryesan58@gmail.com>
2022-06-04 15:48:13 -0400

    s3 error

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 require("dotenv").config();
 
-const S3 = require('aws-sdk')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_key
-const secretAccessKey = process.env.AWS_SECRET_KEY
 
-const s3 = new S3({
-  region,
-  accessKeyId,
-  secretAccessKey
-})
 
 // console.log(process.env.SECRET_KEY, process.env.CLIENT_URL, process.env.PORT)
 
